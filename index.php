90e2702cf39cca9d6bc4567dd6808d95ad2050fa
PolSpock <chao-garden@hotmail.com>
2016-04-19 10:39:25 +0200

    fix du scroll + image size du fil d'actualité

--- index.php
+++ index.php
@@ -33,93 +33,87 @@ session_start();
   </head>
   <body>
 	<div class="container-fluid">
-	 <div class="row headerBar">
-                    <nav class="navbar navbar-default" role="navigation">
-                        <div class="navbar-header">
-                            <button class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
-                                <span class="icon icon-bar"></span>
-                                <span class="icon icon-bar"></span>
-                                <span class="icon icon-bar"></span>
-                            </button>
-                        </div>
-                        <div class="collapse navbar-collapse">
-                            <ul class="nav navbar-nav navbar-left text-uppercase">
-                                <li><a href="index.php">Home</a></li>
-                                <li><a href="#feature">Description</a></li>
-                                <li><a href="#pricing">Team</a></li>
-                                <li><a href="#contact">Contact</a></li>
-                            </ul>
-                            
-                        </div>
-                    </nav>
-            </div>
-            <div class="row menu-left">
-                <div class="col-md-2 menu_left">
-                		<div id="menu">
-                		
-                		<li><a href="#" class="clickLien profil">Profil</a>
-						    <ul>
-						      <li><a href="#">Sous-item 1</a></li>
-						      <li><a href="#">Sous-item 2</a></li>
-						      <li><a href="#">Sous-item 3</a></li>
-						    </ul>
-						</li>
-						
-						</div>
-                	
-                    <a class="clickLien clickShare" href="#share">Upload</a>
-                    <a class="clickLien clickAbsent" href="#page1.php">Absences</a>
-                    <a class="clickLien" href="page1.php">Page 1 </a>
-                    <a class="clickLien" href="page1.php">Page 1 </a>
-                </div>
-                <div class="col-md-10" id="corps">
-                <div style="height:20px;"></div>
-		<div class="row">
-			<div class="col-xs-offset-4 col-sm-offset-4 col-md-offset-4 col-lg-offset-4 col-xs-4 col-sm-4 col-md-4 col-lg-4"><h1 style="text-align:center;">Fil d'actualité</h1></div>
+		<div class="row headerBar">
+			<nav class="navbar navbar-default" role="navigation">
+				<div class="navbar-header">
+					<button class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+						<span class="icon icon-bar"></span>
+						<span class="icon icon-bar"></span>
+						<span class="icon icon-bar"></span>
+					</button>
+				</div>
+				<div class="collapse navbar-collapse">
+					<ul class="nav navbar-nav navbar-left text-uppercase">
+						<li><a href="index.php">Home</a></li>
+						<li><a href="#feature">Description</a></li>
+						<li><a href="#pricing">Team</a></li>
+						<li><a href="#contact">Contact</a></li>
+					</ul>
+					
+				</div>
+			</nav>
 		</div>
-		<?php		
-		foreach($dbh->query('SELECT * FROM wall w left outer join user u ON u.id_user = w.author_wall ORDER BY date_wall DESC') as $row) {
-		?>
-			<div class="row">
-				<div class=""></div>
-				<div class="col-xs-offset-3 col-sm-offset-3 col-md-offset-3 col-lg-offset-3 col-xs-offset-6 col-sm-offset-6 col-md-offset-6 col-lg-offset-6">
-					<p><?php			
-
+        <div class="row menu-left">
+			<div class="col-md-2 menu_left">
+					<div id="menu">
 					
+					<li><a href="#" class="clickLien profil">Profil</a>
+						<ul>
+						  <li><a href="#">Sous-item 1</a></li>
+						  <li><a href="#">Sous-item 2</a></li>
+						  <li><a href="#">Sous-item 3</a></li>
+						</ul>
+					</li>
 					
-					// if (preg_match('/^.*\.(jpg|jpeg|png|gif)$/i', $row['content_wall']))
-						
-					$reg_exUrl = "/(http|https|ftp|ftps)\:\/\/[a-zA-Z0-9\-\.]+\.[a-zA-Z]{2,3}((jpg|jpeg|png|gif)S*)?/";
-					
-					if (preg_match($reg_exUrl, $row['content_wall'])) {
-						$row['content_wall'] = "<img src=" .$row['content_wall'] . ">";
-					}
-						
-						echo $row['alias_user'] . " a publié le " . $row['date_wall'] . " :<br>". $row['content_wall']
-					?></p> 
-				</div>
+					</div>
+				
+				<a class="clickLien clickShare" href="#share">Upload</a>
+				<a class="clickLien clickAbsent" href="#page1.php">Absences</a>
+				<a class="clickLien" href="page1.php">Page 1 </a>
+				<a class="clickLien" href="page1.php">Page 1 </a>
 			</div>
-		<?php
-		}
-		$dbh = null;
-		?>
-		
-		<form action="action.php" method="post">
-			<div class="row" style="text-align:center;">
-				<div class="col-xs-offset-3 col-sm-offset-3 col-md-offset-3 col-lg-offset-3 col-xs-6 col-sm-6 col-md-6 col-lg-6">
-					<textarea name="msg" rows="4" cols="50" placeholder="Decrit au monde a quel point tu es con.."></textarea>
+			<div class="col-md-10" id="corps" style="overflow-y:scroll; overflow-x:hidden; height:100%">
+				<div style="height:20px;"></div>
+				<div class="row">
+					<div class="col-xs-offset-4 col-sm-offset-4 col-md-offset-4 col-lg-offset-4 col-xs-4 col-sm-4 col-md-4 col-lg-4"><h1 style="text-align:center;">Fil d'actualité</h1></div>
 				</div>
-			</div>
-			<div class="row" style="text-align:right;">
-				<div class="col-xs-offset-3 col-sm-offset-3 col-md-offset-3 col-lg-offset-3 col-xs-6 col-sm-6 col-md-6 col-lg-6">
-					<input class="btn btn-primary" name="submit" type="submit" value="submit">
-				</div>
-			</div>
-		</form>
-                </div>
+				<form action="action.php" method="post">
+					<div class="row" style="text-align:center;">
+						<div class="col-xs-offset-3 col-sm-offset-3 col-md-offset-3 col-lg-offset-3 col-xs-6 col-sm-6 col-md-6 col-lg-6">
+							<textarea name="msg" rows="4" cols="50" placeholder="Decrit au monde a quel point tu es con.."></textarea>
+						</div>
+					</div>
+					<div class="row" style="text-align:right;">
+						<div class="col-xs-offset-3 col-sm-offset-3 col-md-offset-3 col-lg-offset-3 col-xs-6 col-sm-6 col-md-6 col-lg-6">
+							<input class="btn btn-primary" name="submit" type="submit" value="submit">
+						</div>
+					</div>
+				</form>
+				<?php		
+				foreach($dbh->query('SELECT * FROM wall w left outer join user u ON u.id_user = w.author_wall ORDER BY date_wall DESC') as $row) {
+				?>
+					<div class="row">
+						<div class=""></div>
+						<div class="col-xs-offset-3 col-sm-offset-3 col-md-offset-3 col-lg-offset-3 col-xs-6 col-sm-6 col-md-6 col-lg-6">
+							<p><?php			
+								// if (preg_match('/^.*\.(jpg|jpeg|png|gif)$/i', $row['content_wall']))
+									
+								$reg_exUrl = "/(http|https|ftp|ftps)\:\/\/[a-zA-Z0-9\-\.]+\.[a-zA-Z]{2,3}((jpg|jpeg|png|gif)S*)?/";
+								
+								if (preg_match($reg_exUrl, $row['content_wall'])) {
+									// $row['content_wall'] = "<img src=" .$row['content_wall'] . ">";
+									$row['content_wall'] = "<img src=" .$row['content_wall']." style=max-width:100%; max-height:100%;>";
+								}
+								echo $row['alias_user'] . " a publié le " . $row['date_wall'] . " :<br>". $row['content_wall']
+							?></p> 
+						</div>
+					</div>
+				<?php
+				}
+				$dbh = null;
+				?>
             </div>
-		
-
+        </div>
 	</div>
 	
     <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
